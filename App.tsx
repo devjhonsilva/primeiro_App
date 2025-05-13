0dd2f1abf7552e209c5ed8ff53c7345788923e56
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-05-13 10:26:00 -0300

    chore: instaled font roboto google

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
+
+import { ThemeProvider } from "styled-components/native";
 import theme from "@theme/theme";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
