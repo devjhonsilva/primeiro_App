c9bd26a08d7e9c1f464120276995962077c44c7e
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-05-14 13:14:07 -0300

    chore: creatd view class to game

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+
+export function Players() {
+  const [team, setTeam] = useState("time a");
+
+  const [players, setPllayer] = useState([
+    "Jonatan",
+    "May",
+    "Jonatan",
+    "May",
+    "Jonatan",
+    "May",
+    "Jonatan",
+    "May",
+    "Jonatan",
+    "May",
+  ]);
+
+  return (
+    <Container>
+      <Header showBackButton />
+      <Highlight
+        title="Nome da turma"
+        subtitle="Adicione a galera e separe os times"
+      />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
+      <HeaderList>
+        <FlatList
+          data={["time a", "time b"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              isActive={item === team}
+              title={item}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 80 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+      <Button title="Remover turma" type="SECONDARY" />
+    </Container>
+  );
+}
